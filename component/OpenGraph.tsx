afe6b54f5c1e122980d4fe1fbfb48430d6932a46
k0102575 <k0102575@gmail.com>
2022-07-14 17:44:35 +0900

    feat: url parmas 테스트

--- component/OpenGraph.tsx
+++ component/OpenGraph.tsx
@@ -10,13 +10,11 @@ const OpenGraph = () => {
   const openGraphData = useMemo(() => {
     const url = asPath.split('?')[0];
 
-    console.log(_.has(OPEN_GRAPH, url))
-
     if (_.has(OPEN_GRAPH, url)) {
-      return {...OPEN_GRAPH[url], url};
+      return { ...OPEN_GRAPH[url], url };
     }
 
-    return {...OPEN_GRAPH.DEFAULT, url};
+    return { ...OPEN_GRAPH.DEFAULT, url };
   }, [asPath]);
 
   return (
@@ -25,7 +23,12 @@ const OpenGraph = () => {
       <meta property="og:title" content={openGraphData.title} />
       <meta property="og:description" content={openGraphData.description} />
       <meta property="og:image" content={openGraphData.image} />
-      <meta property="og:url" content={`https://open-graph-practice.vercel.app`} />
+      <meta
+        property="og:url"
+        content={`https://open-graph-practice.vercel.app${
+          openGraphData.urlParams || ''
+        }`}
+      />
       <meta property="og:site_name" content="Pindirect" />
       <meta property="og:type" content="website" />
       <meta property="og:image:width" content="800" />
@@ -36,7 +39,10 @@ const OpenGraph = () => {
       <meta name="twitter:description" content={openGraphData.description} />
       <meta name="twitter:image" content={openGraphData.image} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={`https://www.open-graph-practice.vercel.app`} />
+      <meta
+        property="twitter:url"
+        content={`https://www.open-graph-practice.vercel.app`}
+      />
       <meta property="twitter:domain" content="pindirectshop.com" />
     </Head>
   );
--- openGraph.ts
+++ openGraph.ts
@@ -2,6 +2,7 @@ export interface OPEN_GRAPH_PROPS {
   title: string;
   description: string;
   image: string;
+  urlParams?: string;
 }
 
 type openGraphTpye = {
@@ -21,7 +22,8 @@ export const OPEN_GRAPH: openGraphTpye = {
   '/ad/z-mini': {
     title: '핀다이렉트 (pindirect)',
     description: 'Z Mini 요금제를 만나보세요',
-    image: 'https://shop-cdn.stagefive.io/z-web/ad/z-mini/z_mini_220714_og.png'
+    image: 'https://shop-cdn.stagefive.io/z-web/ad/z-mini/z_mini_220714_og.png',
+    urlParams: '?utm_campaign=organic&utm_source=pindirect&utm_medium=content_share&utm_content=feeds_1019'
   },
   DEFAULT: {
     title: '핀다이렉트 (pindirect)',
