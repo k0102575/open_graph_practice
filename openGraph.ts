019dc5b34bf7c3da2cbc8b0d4d085517a6be4e9b
k0102575 <k0102575@gmail.com>
2022-07-27 09:40:10 +0900

    feat: k 페이지 추가

--- openGraph.ts
+++ openGraph.ts
@@ -17,12 +17,17 @@ export const OPEN_GRAPH: openGraphTpye = {
   '/ad/z' : {
     title: '핀다이렉트 (pindirect)',
     description: '핀다이렉트Z 요금제를 만나보세요',
-    image: 'https://shop-cdn.stagefive.io/z-web/ad/z-max/z_max_220714_og.png'
+    image: 'https://shop-cdn.stagefive.io/z-web/ad/z/z_220714_og.png'
   },
   '/ad/z-mini': {
     title: '핀다이렉트 (pindirect)',
     description: 'Z Mini 요금제를 만나보세요',
     image: 'https://shop-cdn.stagefive.io/z-web/ad/z-mini/z_mini_220714_og.png',
+  },
+  '/brand/k': {
+    title: '핀다이렉트 (pindirect)',
+    description: 'K 요금제를 만나보세요',
+    image: 'https://shop-cdn.stagefive.io/z-web/brand/k/k_220714_og.png',
   },
   DEFAULT: {
     title: '핀다이렉트 (pindirect)',
